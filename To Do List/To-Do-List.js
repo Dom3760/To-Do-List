99dd491d8a3f788bc960334e18b9667631c95bd8
Dom3760 <142520274+Dom3760@users.noreply.github.com>
2023-10-13 10:24:14 -0700

    Add files via upload

---
+++ To Do List/To-Do-List.js
@@ -0,0 +1,6 @@
+const input = document.getElementById("whatToDo");
+const addButton = document.getElementById("addButton");
+const divContainer = document.getElementById("items");
+var items = document.createElement("p");
+items.textContent = "this is sample text";
+divContainer.appendChild(items);
